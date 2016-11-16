8054f4ebbfb2837a610d87dd94c1d8203fd818c5
lgnexus4fun <lgnexus4fun@gmail.com>
2016-11-16 17:51:48 +0200

    Fix create post form.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,9 +8,12 @@
                     <div class="panel-heading">Create post</div>
                     <div class="panel-body">
                         <form method="post" action="{{ url('/posts') }}">
+
                             {{ csrf_field() }}
+
                             <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
                                 <label for="title">Title</label>
+
                                 <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}" required autofocus>
 
                                 @if ($errors->has('title'))
@@ -19,9 +22,13 @@
                                     </span>
                                 @endif
                             </div>
+
                             <div class="form-group{{ $errors->has('excerpt') ? ' has-error' : '' }}">
                                 <label for="excerpt">Excerpt</label>
-                                <textarea class="form-control" id="excerpt" rows="2" name="excerpt" required>{{ old('excerpt') }}</textarea>
+
+                                <textarea class="form-control" id="excerpt" rows="2" name="excerpt" required>
+                                    {{ old('excerpt') }}
+                                </textarea>
 
                                 @if ($errors->has('excerpt'))
                                     <span class="help-block">
@@ -29,9 +36,13 @@
                                     </span>
                                 @endif
                             </div>
+
                             <div class="form-group{{ $errors->has('body') ? ' has-error' : '' }}">
                                 <label for="body">Body</label>
-                                <textarea class="form-control" id="body" rows="4" name="body" required>{{ old('body') }}</textarea>
+
+                                <textarea class="form-control" id="body" rows="5" name="body" required>
+                                    {{ old('body') }}
+                                </textarea>
 
                                 @if ($errors->has('body'))
                                     <span class="help-block">
@@ -39,6 +50,7 @@
                                     </span>
                                 @endif
                             </div>
+
                             <button type="submit" class="btn btn-primary">Create</button>
                         </form>
                     </div>
