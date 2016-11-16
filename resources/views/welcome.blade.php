71d5c41830a91c7c48cb2dd7c482c6be8a06afd4
lgnexus4fun <lgnexus4fun@gmail.com>
2016-11-16 18:34:05 +0200

    Add blog link on welcome page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,6 +84,7 @@
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ url('/posts') }}">Blog</a>
                 </div>
             </div>
         </div>
