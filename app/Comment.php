3910c5bed7c52b183afd7d66c5d54b81391b5c97
lgnexus4fun <lgnexus4fun@gmail.com>
2016-11-29 20:12:53 +0200

    Add Comment.

---
+++ app/Comment.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['body'];
+
+    /**
+     * A comment has an owner.
+     *
+     * @return BelongsTo
+     */
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Use a custom collection for all comments.
+     *
+     * @param  array  $models
+     * @return CommentCollection
+     */
+    public function newCollection(array $models = [])
+    {
+        return new CommentCollection($models);
+    }
+}
