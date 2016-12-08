f3a121cd6efb17854bcb696aa14f1b95855b0ab2
lgnexus4fun <lgnexus4fun@gmail.com>
2016-12-08 13:01:49 +0200

    Remove auth middleware from search action.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,7 +19,7 @@ class PostController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['index', 'show']]);
+        $this->middleware('auth', ['except' => ['index', 'show', 'search']]);
     }
 
     /**
